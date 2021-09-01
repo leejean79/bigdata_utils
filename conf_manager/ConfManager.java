6ae268987a0b94ba66f8c06236bc04d00a9d3a4a
leejean79 <39007870+leejean79@users.noreply.github.com>
2021-09-01 11:47:44 +0800

    Update ConfManager.java

--- conf_manager/ConfManager.java
+++ conf_manager/ConfManager.java
@@ -18,14 +18,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
+/*
+通过内部静态类创建配置文件的单例模式，保证多线程中的唯一性
+*/
 public class ConfManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfManager.class);
 
     public static ServerConfig serverConfig;
 
-    //public static Configuration hbaseConfig;
-
+    /*
+    创建一个map，key：配置文件名，value：配置文件的对象
+    */
     public static Map<String, TopicConfig> topicConfigMap;
 
     static {
@@ -33,17 +37,29 @@ public class ConfManager {
         serverConfig = new ServerConfig(Utils.loadPropsByCL("client.properties", ConfManager.class));
     }
 
+    /*
+    私有化构造函数
+    */
     private ConfManager() {
     }
 
+    /*
+    定义私有静态内部类，返回外部类的实例
+    */
     private static class SingletonHelper {
         private static final ConfManager INSTANCE = new ConfManager();
     }
 
+    /*
+    单例模式
+    */
     public static ConfManager getInstance() {
         return SingletonHelper. INSTANCE;
     }
 
+    /*
+    封装对象的方法
+    */
     private void load(String name, String configJson) throws ServiceException {
 
         org.boon.json.ObjectMapper mapper = JsonFactory.create();
@@ -60,18 +76,23 @@ public class ConfManager {
 
     }
 
+    /*
+    转换的主要过程
+    */
     private void initFromFile() throws IOException {
 
+//         1. 读取工程根路径resource下的conf文件夹下的配置文件名
         InputStream dirInputStream = ConfManager.class.getClassLoader().getResourceAsStream("conf");
 
         BufferedReader dirReader = new BufferedReader(new InputStreamReader(dirInputStream));
         String configFileName;
-
+//          2. 如果存在配置文件，则通过正则提取文件
         while ((configFileName = dirReader.readLine()) != null) {
             LOGGER.error("ConfigFileNam=" + configFileName);
             Pattern configFilePattern = Pattern.compile("event\\-\\w+\\.json");
             Matcher configFileMatcher = configFilePattern.matcher(configFileName);
             if (configFileMatcher.find()) {
+//                 3. 读取配置文件的内容
                 InputStream configFileStream = ConfManager.class.getClassLoader()
                         .getResourceAsStream("conf/" + configFileName);
                 BufferedReader configReader = new BufferedReader(new InputStreamReader(configFileStream));
@@ -81,7 +102,7 @@ public class ConfManager {
                     configJson.append(configLineInfo);
                 }
                 try {
-
+//                  4. 将该json文件转为对应的对象
                     load(configFileName.substring(0, configFileName.indexOf(".")), configJson.toString());
 
                 } catch (ServiceException e) {
